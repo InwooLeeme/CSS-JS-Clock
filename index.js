fdf4b5970e1f4b42bcecf27ebae9cb65f35d20b6
ItsmeLee <arsmagica90@gmail.com>
2021-01-07 00:17:11 +0900

    Rotate seconds bar
    The second hand spins to the second.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 function getTime(){
+    const hourBar = document.querySelector('.hours');
+    const minuteBar = document.querySelector('.minutes');
+    const secondBar = document.querySelector('.seconds');
     const nowDate = new Date();
     const hours = nowDate.getHours();
     const minutes = nowDate.getMinutes();
     const seconds = nowDate.getSeconds();
-    console.log(hours, minutes, seconds);
+    
+    const secTransDeg = seconds * 6;
+    secondBar.style.transform = `rotate(${secTransDeg}deg)`;
+    console.log(secondBar.style.transform);
 }
 
 function init(){
